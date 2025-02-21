77661ab115c9e511e588a7559db01f78bc35450a
Arin <117519049+aringuzel@users.noreply.github.com>
2025-02-21 17:10:11 +0000

    Update login.js

--- backend/login.js
+++ backend/login.js
@@ -7,11 +7,11 @@ import { getAuth, signInWithEmailAndPassword, sendPasswordResetEmail } from "htt
 const firebaseConfig = {
 
     apiKey: "",
-    authDomain: "share-ideas-af309.firebaseapp.com",
-    projectId: "share-ideas-af309",
-    storageBucket: "share-ideas-af309.firebasestorage.app",
-    messagingSenderId: "757953988207",
-    appId: "1:757953988207:web:fe8a929c63bac73fd66f9a"
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   
 };
 
